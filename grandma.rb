cbf29646ed505bcf4d764e7c1aa01c4ed1505744
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-13 05:19:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,4 @@
 def speak_to_grandma(say.upcase)
 if say == "
-    puts "I love you too pumpkin!"
-    elsif say =="Hi nana, how are you?"
-    puts "Huh? speak up sonny!"
-    elsif say =="Hi!"
-    puts "Huh? speak up sonny!"
-    elsif say == "What did you eat today?"
-    puts "No, not since 1938!"
-  else say == "What?"
-    puts "No, not since 1938!"
-  end
+    
 
