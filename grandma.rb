47206f5cd112e2cf0d560c993f9f341beb55a852
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-13 05:44:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,8 @@ if phrase == "I LOVE YOU GRANDMA!"
  return "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == phrase.upcase
  return "NO, NOT SINCE 1938!"
+ else 
+   return "I LOVE YOU TOO PUMPKIN!"
+ end
     
 
